5137225e6f7ffbca23bdb2c875f2ce1372288f5b
rua-iri <rory.mcguigan@protonmail.com>
2024-08-11 21:48:31 +0300

    update: improve error handling

--- terminal_radio/helpers.py
+++ terminal_radio/helpers.py
@@ -2,10 +2,16 @@ import climage
 
 
 def initialise_logs(file_name: str):
-    with open(file_name, "a") as file:
-        file.write("\n\n-------------------------\n")
+    try:
+        with open(file_name, "a") as file:
+            file.write("\n\n-------------------------\n")
+    except Exception as e:
+        raise e
 
 
 def load_station_logo(img_src: str) -> str:
-    img_output = climage.convert(img_src)
-    return img_output
+    try:
+        img_output = climage.convert(img_src)
+        return img_output
+    except Exception as e:
+        raise e
