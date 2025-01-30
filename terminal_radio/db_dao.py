990d1cbea103cdc7e780092aaff15f7df8c23772
rua-iri <rory.mcguigan@protonmail.com>
2025-01-30 09:21:16 +0300

    update: complete crud functionality on DAO

--- terminal_radio/db_dao.py
+++ terminal_radio/db_dao.py
@@ -8,20 +8,52 @@ class DB_DAO:
         connection.row_factory = sqlite3.Row
         self.cursor = connection.cursor()
 
-    def select_all_stations(self):
+    def select_all_stations(self) -> list[sqlite3.Row]:
         query_string: str = "SELECT id, name FROM stations"
         result = self.cursor.execute(query_string)
         return result.fetchall()
 
-    def select_station_by_id(self, id: int):
-        print(1)
-        query_string: str = "SELECT * FROM stations WHERE id="
-        print(2)
+    def select_station_by_id(self, id: int) -> sqlite3.Row:
+        query_string: str = "SELECT * FROM stations WHERE id=?"
         result = self.cursor.execute(query_string, (id, ))
-        print(3)
         return result.fetchone()
 
+    def update_station(self,
+                       id: int,
+                       name: str,
+                       url: str,
+                       img: str,
+                       is_yt: bool,
+                       is_active: bool
+                       ):
+        query_string: str = """UPDATE stations
+        SET name=?, url=?, img=?, is_yt=?, is_active=?
+        WHERE id=?"""
 
+        self.cursor.execute(
+            query_string,
+            (name, url, img, is_yt, is_active, id, )
+        )
+
+    def delete_station(self, id: int):
+        query_string: str = "DELETE FROM stations WHERE id=?"
+
+        self.cursor.execute(query_string, (id, ))
+
+    def create_station(self,
+                       name: str,
+                       url: str,
+                       img: str,
+                       is_yt: bool,
+                       ):
+        query_string: str = """INSERT INTO
+        stations (name, url, img, is_yt, is_active)
+        VALUES (?, ?, ?, ?, ?)"""
+
+        self.cursor.execute(query_string, (name, url, img, is_yt, True))
+
+
+# For testing, remove later
 a = DB_DAO()
 
 x = a.select_all_stations()
@@ -30,3 +62,4 @@ for z in x:
 
 y = a.select_station_by_id(id=10)
 print(y)
+print({**y})
