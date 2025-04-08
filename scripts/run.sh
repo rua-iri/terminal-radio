8dde00f09f3cdbe48b16c536410142c86988ac92
rua-iri <rory.mcguigan@protonmail.com>
2025-04-08 12:49:14 +0300

    update: run script to accept command line arguments

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,16 @@
 cd /opt/terminal_radio
 
+if [ ! -z $1 ]; then
+    CMD='play'
+else
+    CMD=$1
+fi
+
+
 
 VENV=./.venv
 PYTHON=$VENV/bin/python3
 PIP=$VENV/bin/pip3
 
-$PYTHON main.py play
+$PYTHON main.py $CMD
 
