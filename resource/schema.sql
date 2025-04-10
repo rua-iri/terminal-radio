9c831240120fe289e89b6d55d5f25b37f69f78b7
rua-iri <rory.mcguigan@protonmail.com>
2025-04-10 16:21:55 +0300

    fix: default station inactive

--- resource/schema.sql
+++ resource/schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS "stations" (
 	"is_active"	INTEGER NOT NULL,
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
-INSERT INTO stations VALUES(1,'BBC World Service','https://a.files.bbci.co.uk/ms6/live/3441A116-B12E-4D2F-ACA8-C1984642FA4B/audio/simulcast/hls/nonuk/audio_syndication_low_sbr_v1/cfs/bbc_world_service.m3u8','https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/BBC_World_Service_2022_(Boxed).svg/500px-BBC_World_Service_2022_(Boxed).svg.png',0,0);
+INSERT INTO stations VALUES(1,'BBC World Service','https://a.files.bbci.co.uk/ms6/live/3441A116-B12E-4D2F-ACA8-C1984642FA4B/audio/simulcast/hls/nonuk/audio_syndication_low_sbr_v1/cfs/bbc_world_service.m3u8','https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/BBC_World_Service_2022_(Boxed).svg/500px-BBC_World_Service_2022_(Boxed).svg.png',0,1);
 CREATE TABLE IF NOT EXISTS "last_station" (
 	"id"	INTEGER UNIQUE,
 	"station_id"	INTEGER,
