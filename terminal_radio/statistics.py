39c1eac9ca065e880025a00b48aba49c4e12606b
rua-iri <rory.mcguigan@protonmail.com>
2025-04-30 10:41:38 +0300

    add: statistics file

---
+++ terminal_radio/statistics.py
@@ -0,0 +1,19 @@
+
+
+from terminal_radio.db_dao import DB_DAO
+from tabulate import tabulate
+
+
+def show_table(stats_list: list):
+    headers = stats_list[0].keys()
+    table = tabulate(stats_list, headers=headers)
+    print(table)
+
+
+def main():
+    db_dao = DB_DAO()
+    top_5_stations = db_dao.get_stats_top_5()
+
+    show_table(top_5_stations)
+
+    db_dao.close()
