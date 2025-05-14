00d3f66133e6dbf734f0f67a4d23801bf9ab9578
rua-iri <rory.mcguigan@protonmail.com>
2025-05-14 13:57:30 +0300

    update: exclude more files from being copied to installation directory

--- scripts/install.sh
+++ scripts/install.sh
@@ -49,9 +49,17 @@ printf "Done\n\n"
 # symbolic link to add to $PATH
 printf "Moving to Installation Directory: "
 
-# prevent overwriting the database
+
+# prevent overwriting the important files and copying unnecesary files
 if [ -f /opt/terminal_radio/resource/radio_sources.sqlite ]; then 
-    sudo rsync -av --exclude='resource/radio_sources.sqlite' . /opt/terminal_radio
+    EXCLUDE_DIRS=(
+    --exclude='resource/radio_sources.sqlite' 
+    --exclude='logs/' 
+    --exclude='__pycache__/' 
+    --exclude='resource/config.yaml'
+    )
+
+    sudo rsync -av "${EXCLUDE_DIRS[@]}" . /opt/terminal_radio
 else
     sudo rsync -av . /opt/terminal_radio
 fi
