7a4090ab1b25da85d2d31dbddb8fd79599a2455e
rua-iri <rory.mcguigan@protonmail.com>
2025-05-14 11:58:28 +0300

    update: minor linting for statistics file

--- terminal_radio/statistics.py
+++ terminal_radio/statistics.py
@@ -1,10 +1,14 @@
 
-
-from terminal_radio.db_dao import DB_DAO
 from tabulate import tabulate
+from terminal_radio.db_dao import DB_DAO
 
 
 def show_table(stats_list: list):
+    """Show a Table of user statistics
+
+    Args:
+        stats_list (list): A list of the top 5 stations for a user
+    """
     headers = stats_list[0].keys()
     table = tabulate(stats_list, headers=headers, tablefmt="fancy_grid")
     print(table)
