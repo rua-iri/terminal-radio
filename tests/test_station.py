24b640a3dc9d198655734d1d999fa607f90b39bf
rua-iri <rory.mcguigan@protonmail.com>
2025-06-18 17:12:18 +0300

    update: station tests

--- tests/test_station.py
+++ tests/test_station.py
@@ -6,7 +6,10 @@ from terminal_radio.classes import Station
 def test_create_station():
     assert True is True
     station = Station(
-        "Test Station", "https://example.com", "https://example.com", False
+        name="Test Station",
+        url="https://example.com",
+        img="https://example.com",
+        is_yt=False
     )
 
     assert type(station) is Station
@@ -15,3 +18,19 @@ def test_create_station():
     assert station.name == "Test Station"
     assert station.url == "https://example.com"
     assert station.img == "https://example.com"
+
+
+def test_create_station_yt():
+    initial_station_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    station = Station(
+        name="Test Station",
+        url=initial_station_url,
+        img="",
+        is_yt=True
+    )
+
+    print(station.url)
+
+    assert station.url != initial_station_url
+    assert station.img != ""
+    assert station.url.startswith("https://manifest.googlevideo.com")
