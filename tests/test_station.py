43b596e2f645dcc24251bd94acb514a1a5bc1093
rua-iri <rory.mcguigan@protonmail.com>
2025-06-18 10:07:11 +0300

    add: tests for station

--- tests/test_station.py
+++ tests/test_station.py
@@ -1,1 +1,17 @@
-import unittest
+import pytest
+
+from terminal_radio.classes import Station
+
+
+def test_create_station():
+    assert True is True
+    station = Station(
+        "Test Station", "https://example.com", "https://example.com", False
+    )
+
+    assert type(station) is Station
+
+    assert station.is_yt is False
+    assert station.name == "Test Station"
+    assert station.url == "https://example.com"
+    assert station.img == "https://example.com"
