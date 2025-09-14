99e662e7bf853d9efdf42e3c7b3979343d8a4cb4
rua-iri <rory.mcguigan@protonmail.com>
2025-09-14 11:01:13 +0300

    add: BubbleTea Dependency for interactive lists

--- internal/radio/radio.go
+++ internal/radio/radio.go
@@ -2,12 +2,31 @@ package radio
 
 import (
 	"fmt"
+	"reflect"
+
+	"github.com/charmbracelet/bubbles/list"
 )
 
 func play_radio() {
 	fmt.Println("Playing Radio...")
+	items := []list.Item{
+		item("Ramen"),
+		item("Tomato Soup"),
+		item("Hamburgers"),
+		item("Cheeseburgers"),
+		item("Currywurst"),
+		item("Okonomiyaki"),
+		item("Pasta"),
+		item("Fillet Mignon"),
+		item("Caviar"),
+		item("Just Wine"),
+	}
+	var answer string = MainMenu(items)
+
+	fmt.Println("'", answer, "'")
+	fmt.Println(reflect.TypeOf(answer))
 }
 
 func Main() {
-
+	play_radio()
 }
